4c3718701e86c2750a41bbe273004960871a567e
akop4 <akopov.andrew@gmail.com>
2023-12-30 13:39:29 +0300

    user credintials script

---
+++ user_credintials.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+exec < /dev/tty7 > /dev/tty7
+chvt 7
+echo -n "Input user Name: "
+read USERNAME
+# While loop for password
+echo -n "Enter user " 
+USERPASSWD=$(mkpasswd -m sha512crypt)
+chvt 1
+echo "user --name=\"$USERNAME\" --groups=wheel --password=\"$USERPASSWD\" --iscrypted" # >> /tmp/user_data
+#printf "user --name=\"$USERNAME\" --groups=wheel --password=\"$USERPASSWD\" --iscrypted" # >> /tmp/user_data
+
